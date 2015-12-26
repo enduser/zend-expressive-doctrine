b5c19d84dc18a7e4322caf3549e94637e25438d0
Endin Artyom <artyom.endin@gmail.com>
2015-12-26 13:23:33 +0300

    first commit

---
+++ src/ZendExpressiveDoctrine.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace Novomirskoy\Zend\Expressive\Doctrine;
+
+class ZendExpressiveDoctrine
+{
+
+    /**
+     * Initialize.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+
+    }
+}
---
+++ tests/ZendExpressiveDoctrineTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Novomirskoy\Zend\Expressive\Doctrine\Tests;
+
+use Novomirskoy\Zend\Expressive\Doctrine\ZendExpressiveDoctrine;
+use PHPUnit_Framework_TestCase as PHPUnit;
+
+class ZendExpressiveDoctrineTest extends PHPUnit
+{
+    public function testExample()
+    {
+        $zendExpressiveDoctrine = new ZendExpressiveDoctrine();
+
+        $this->assertTrue(true);
+    }
+}
